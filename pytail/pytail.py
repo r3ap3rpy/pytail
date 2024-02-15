c92841e95443725eb574a10cb2878afb8c23fa18
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2024-02-15 10:02:36 +0100

    Updated script to deal with file access issues

--- pytail/pytail.py
+++ pytail/pytail.py
@@ -1,21 +1,26 @@
 from time import sleep
-
+from errno import EACCES
 def tail(file, fltr):
-    with open(file) as tailed:
-        tailed.seek(0,2)
-        while True:
-            try:
-                line = tailed.readline()
-                if line:
-                    if fltr:
-                        if not fltr.upper() in line.upper():
-                            continue
-                    yield line.strip()
-
-
-                sleep(.1)
-            except KeyboardInterrupt:
-                print("\r" + ("#" * 30))
-                print("# End of tailing!")
-                break
-
+    try:
+        with open(file) as tailed:
+            tailed.seek(0,2)
+            while True:
+                try:
+                    line = tailed.readline()
+                    if line:
+                        if fltr:
+                            if not fltr.upper() in line.upper():
+                                continue
+                        yield line.strip()
+                    sleep(.1)
+                except KeyboardInterrupt:
+                    print("\r" + ("#" * 30))
+                    print("# End of tailing!")
+                    break
+    except IOError as x:
+        if x.errno == EACCES:
+            print(f"# The specified file: {file} cannot be read!")
+        else:
+            print(f"# Other error: {x}")
+        print("#"*30)
+        exit(-1)
